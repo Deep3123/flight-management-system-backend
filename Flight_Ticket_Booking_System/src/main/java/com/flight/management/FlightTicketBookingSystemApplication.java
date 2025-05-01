daa8915bc6a6c0448908c8219e713fefd9ac6049
Deep Patel <deep.patel@oplinnovate.com>
2025-05-01 12:35:33 +0530

    Added and changed some new files.

--- Flight_Ticket_Booking_System/src/main/java/com/flight/management/FlightTicketBookingSystemApplication.java
+++ Flight_Ticket_Booking_System/src/main/java/com/flight/management/FlightTicketBookingSystemApplication.java
@@ -7,9 +7,9 @@ import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class FlightTicketBookingSystemApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		// Dotenv dotenv = Dotenv.load();
 
-		System.out.println("Mongo URI: " + dotenv.get("SPRING_DATA_MONGODB_URI"));
+		// System.out.println("Mongo URI: " + dotenv.get("SPRING_DATA_MONGODB_URI"));
 
 		SpringApplication.run(FlightTicketBookingSystemApplication.class, args);
 	}
